7dffbc62c60222fd06ef2167fce32dda6792f8ad
OwaisTaha <owaistaha67@gmail.com>
2020-04-07 20:52:24 +0500

    initial commit

---
+++ app/src/main/java/com/example/firbasequiz/UserProfile.java
@@ -0,0 +1,16 @@
+package com.example.firbasequiz;
+
+public class UserProfile {
+
+    public String name, email, phone;
+
+    public UserProfile(){
+
+    }
+
+    public UserProfile(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
---
+++ app/src/main/java/com/example/firbasequiz/test.java
@@ -0,0 +1,180 @@
+package com.example.firbasequiz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = "TAG";
+    private EditText editTextName, editTextEmail, editTextPassword, editTextPhone;
+    private ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String UserID;
+    private Button btnalreadylog;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        btnalreadylog = findViewById(R.id.btnalreadylog);
+        editTextName = findViewById(R.id.edit_text_name);
+        editTextEmail = findViewById(R.id.edit_text_email);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        editTextPhone = findViewById(R.id.edit_text_phone);
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
+        btnalreadylog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(test.this, login_activity.class));
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        findViewById(R.id.button_register).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+            //handle the already login user
+        }
+    }
+
+    private void registerUser() {
+        final String name = editTextName.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        final String phone = editTextPhone.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            editTextName.setError(getString(R.string.input_error_name));
+            editTextName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError(getString(R.string.input_error_email));
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError(getString(R.string.input_error_email_invalid));
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError(getString(R.string.input_error_password));
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError(getString(R.string.input_error_password_length));
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (phone.isEmpty()) {
+            editTextPhone.setError(getString(R.string.input_error_phone));
+            editTextPhone.requestFocus();
+            return;
+        }
+
+        if (phone.length() != 10) {
+            editTextPhone.setError(getString(R.string.input_error_phone_invalid));
+            editTextPhone.requestFocus();
+            return;
+        }
+
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                                    progressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(test.this, getString(R.string.registration_success), Toast.LENGTH_LONG).show();
+                                        UserID = mAuth.getCurrentUser().getUid();
+                                        DocumentReference documentReference = fStore.collection("users").document(UserID);
+                                        Map<String,Object> user = new HashMap<>();
+                                        user.put("fName",name);
+                                        user.put("email",email);
+                                        user.put("phone",phone);
+
+                                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "onSuccess: user Profile is created for "+ UserID);
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.d(TAG, "onFailure: " + e.toString());
+                                            }
+                                        });
+                                    }
+
+                                    startActivity(new Intent(test.this , login_activity.class));
+
+                                } else {
+                            Toast.makeText(test.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_register:
+                registerUser();
+                break;
+        }
+
+
+    }
+}
